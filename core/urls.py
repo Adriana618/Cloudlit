b201abebb68768415d96fd8a00d5a82768a6eb1f
Renato <renato@bitmaker.la>
2021-11-30 19:20:54 -0500

    add base template

--- core/urls.py
+++ core/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 
 from core import views
 
-urlpatterns = [path("python", views.execute)]
+urlpatterns = [
+    path("", views.login),
+    path("python", views.execute),
+]
